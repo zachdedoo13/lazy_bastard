5180ad6972514eee6e655ce7d036023edcf4c86a
zacha <zachary.campbell2006@outlook.com>
2024-10-10 19:02:40 +1100

    updateing

--- examples/mt.rs
+++ examples/mt.rs
@@ -1,4 +1,3 @@
-use std::marker::PhantomData;
 use lazy_bastard::{lazy_bastard, time_section_print};
 
 fn main() {
@@ -17,13 +16,10 @@ fn main() {
 lazy_bastard!(
    #[derive(Clone, Debug)]
    pub struct MyStruct<'a> {
-      field1: i32 => 100_324,
-      field2: String => { "test".into() },
-      field3: f64,
-
-      phantom: PhantomData<&'a ()> => PhantomData::default(),
-
-      long: f32 => {
+      normal: i32 => 100_324,
+      function_call: String => "test".into(),
+      automatic: f64, // uses Default::default() instead
+      scoped: f32 => {
          let c: f32 = 1.2;
          let v = c.abs().sin().sin().sqrt();
          0.1 * v
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,59 @@
+//! inline defaults for struct definitions if you're lazy, plus some other stuff, probably.
+//!
+//! makes creating simple structs that don't need a constructor like settings structs much clearer
+//!
+//! somewhat second guessing the name, but it's too late to change it
+//!
+//! ```
+//! use lazy_bastard::lazy_bastard;
+//!
+//! lazy_bastard!(
+//!    #[derive(Clone, Debug)]
+//!    pub struct MyStruct<'a> { // lifetime is just for the example
+//!       normal: i32 => 100_324,
+//!       function_call: String => "test".into(),
+//!       automatic: f64, // uses Default::default() instead
+//!       scoped: f32 => {
+//!          let c: f32 = 1.2;
+//!          let v = c.abs().sin().sin().sqrt();
+//!          0.1 * v
+//!       }
+//!    }
+//! );
+//!
+//!
+//! impl MyStruct {
+//!     fn new() -> Self { Self::default() }
+//! }
+//!
+//!
+//! // Compiles into //
+//!
+//! #[derive(Clone, Debug)]
+//! pub struct CompiledStruct<'a> {
+//!    normal: i32,
+//!    function_call: String,
+//!    automatic: f64,
+//!    scoped: f32,
+//! }
+//! impl<'a> Default for CompiledStruct<'a> {
+//!    fn default() -> Self {
+//!       Self {
+//!          normal: 100_324,
+//!          function_call: ("test".into()),
+//!          automatic: Default::default(),
+//!          scoped: {
+//!             let c: f32 = 1.2;
+//!             let v = c.abs().sin().sin().sqrt();
+//!             0.1 * v
+//!          },
+//!       }
+//!    }
+//! }
+//!
+//! ```
 
-
-/// inline defaults for struct definitions because your lazy
+/// inline defaults for struct definitions if your lazy
 /// ```
 /// use lazy_bastard::lazy_bastard;
 ///
